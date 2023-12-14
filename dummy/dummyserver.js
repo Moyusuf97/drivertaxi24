594ec933e2504ab463bb2c58ea4fc30a888daf35
zakariye <zakariye1942@icloud.com>
2023-12-14 14:20:07 +0100

    yes

--- dummy/dummyserver.js
+++ dummy/dummyserver.js
@@ -4,31 +4,54 @@ const port = 4000;
 
 app.use(express.json());
 
-const userData = require('./user.json');
-const driverData = require('./driver.json');
-const headquartersData = require('./headquarters.json');
+const data = require('./data.json');
 
 function calculateDistance(location1, location2) {
-    const str1 = String(location1);
-    const str2 = String(location2);
-  
-    return Math.abs(str1.charCodeAt(0) - str2.charCodeAt(0));
+  const lat1 = location1.latitude;
+  const lon1 = location1.longitude;
+  const lat2 = location2.latitude;
+  const lon2 = location2.longitude;
+
+  // Check if any of the location properties are undefined
+  if (lat1 === undefined || lon1 === undefined || lat2 === undefined || lon2 === undefined) {
+    throw new Error('Invalid location data');
   }
-  
-  
-  
 
+  // Using Haversine formula to calculate distance between two points on the Earth
+  const R = 6371; // Radius of the Earth in kilometers
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+
+  return distance;
+}
 app.post('/request-ride', (req, res) => {
-  const userRequest = req.body;
+  console.log('Request Body:', req.body);  // Add this line
 
-  const availableDrivers = headquartersData.map.cars.filter((car) => car.isAvailable);
+  const userRequest = req.body;
+  // Check if the location property is present in the user request
+  if (!userRequest.location || !userRequest.location.latitude || !userRequest.location.longitude) {
+    res.status(400).json({ message: 'Invalid user location data' });
+    return;
+  }
+
+  const availableDrivers = data.drivers.filter((driver) => driver.isAvailable);
 
   if (availableDrivers.length > 0) {
     const selectedDriver = availableDrivers.reduce((prev, curr) => {
+      // Check if the location property is present in both prev and curr
+      if (!prev.location || !prev.location.latitude || !prev.location.longitude ||
+          !curr.location || !curr.location.latitude || !curr.location.longitude) {
+        throw new Error('Invalid driver location data');
+      }
+
       const prevDistance = calculateDistance(prev.location, userRequest.location);
       const currDistance = calculateDistance(curr.location, userRequest.location);
 
-   
       if (prevDistance < currDistance) {
         return prev;
       } else if (currDistance < prevDistance) {
@@ -52,7 +75,7 @@ app.post('/request-ride', (req, res) => {
 });
 
 app.get('/drivers', (req, res) => {
-  res.json(driverData);
+  res.json(data.drivers);
 });
 
 app.listen(port, () => {
