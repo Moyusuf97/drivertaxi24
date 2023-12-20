80750a150f778ebba1c3989458d9cdb8893fd9d6
muza97 <mustafa.zaitoun97@gmail.com>
2023-12-20 13:51:06 +0100

    Realtime-database

--- server.js
+++ server.js
@@ -1,47 +1,102 @@
 require('dotenv').config();
-
-
 const express = require('express');
 const admin = require('firebase-admin');
-const User = require('./models/user')
-const initializeFirebaseAdmin = require('./config/firebasedatabase');
-const authRoutes = require('./authRoutes');
-const checkToken = require('./authMiddleware');
+const serviceAccount = require('./RealTime-data.json');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-
-
-
-initializeFirebaseAdmin();
-
-
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/api/auth', authRoutes);
-
-
-app.get('/api/profile', checkToken, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password'); 
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://fir-realtime-339a1-default-rtdb.europe-west1.firebasedatabase.app',
 });
 
+
+
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
+  
+
+
+
+
+// Endpoint to retrieve data
 app.get('/getUser', async (req, res) => {
+    try {
+      const snapshot = await admin.database().ref('/users').once('value');
+      const userData = snapshot.val();
+      res.json(userData);
+    } catch (error) {
+      console.error('Error retrieving data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+// Endpoint to register users
+app.post('/registerUser', async (req, res) => {
   try {
-    const snapshot = await admin.database().ref('/user').once('value');
-    const userData = snapshot.val();
-    res.json(userData);
+    const { email, password } = req.body;
+
+    // Implement user registration logic using Firebase Authentication
+    // Note: This is a simplified example; make sure to add proper error handling and validation
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+    });
+
+    // Save additional user data to Realtime Database
+    await admin.database().ref(`/users/${userRecord.uid}`).set({
+      email,
+      // Add more user data as needed
+    });
+
+    res.status(201).json({ msg: 'User registered successfully' });
   } catch (error) {
-    console.error('Error retrieving data:', error);
+    console.error('Error registering user:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+app.post('/registerGoogleUser', async (req, res) => {
+    try {
+      const { idToken } = req.body;
+      console.log('Received idToken:', idToken);
+  
+      // Verify Google ID token
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      const { uid, email } = decodedToken;
+  
+      // Save additional user data to Realtime Database
+      await admin.database().ref(`/users/${uid}`).set({
+        email,
+        // Add more user data as needed
+      });
+  
+      console.log('User registered successfully');
+      res.status(201).json({ msg: 'User registered successfully' });
+    } catch (error) {
+      console.error('Error registering user:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
