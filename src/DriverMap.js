c96422a2c97fd9452e3e274ee308ad8cc0d4ab88
Mohamed Yusuf <momme40@hotmail.com>
2024-01-22 14:04:18 +0100

    the location of the driver

--- src/DriverMap.js
+++ src/DriverMap.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker, Circle } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, Polygon } from '@react-google-maps/api';
 
 const containerStyle = { width: '100%', height: '400px' };
 
 const DriverMapComponent = () => {
   const [mapCenter, setMapCenter] = useState(null);
   const [driverMarker, setDriverMarker] = useState(null);
-  const [error, setError] = useState('');
   const apiKey = process.env.REACT_APP_ACCESS_KEY;
-  const zoneRadius = 1000; // Radius in meters
 
   useEffect(() => {
     const watchId = navigator.geolocation.watchPosition(
@@ -19,38 +17,41 @@ const DriverMapComponent = () => {
       },
       (err) => {
         console.error(err);
-        setError('Failed to fetch location.');
+        setMapCenter({ lat: 59.334591, lng: 18.063240 }); 
       },
       { enableHighAccuracy: true }
     );
 
-    return () => {
-      navigator.geolocation.clearWatch(watchId);
-    };
+    return () => navigator.geolocation.clearWatch(watchId);
   }, []);
 
+  // Define the points in the polygon
+  const zonePolygonPath = [
+    { lat: 59.340597, lng: 18.058278 }, 
+    { lat: 59.340597, lng: 18.068303 }, 
+    { lat: 59.330570, lng: 18.068303 }, 
+    { lat: 59.330570, lng: 18.058278 }  
+  ];
+
+  // Polygon options
+  const polygonOptions = {
+    fillColor: "lightblue",
+    fillOpacity: 0.4,
+    strokeColor: "blue",
+    strokeOpacity: 0.8,
+    strokeWeight: 2
+  };
+
   return (
-    mapCenter && (
-      <LoadScript googleMapsApiKey={apiKey}>
-        <GoogleMap mapContainerStyle={containerStyle} center={mapCenter} zoom={14}>
-          {driverMarker && <Marker position={driverMarker} />}
-          {driverMarker && (
-            <Circle
-              center={driverMarker}
-              radius={zoneRadius}
-              options={{
-                strokeColor: '#ff0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#ff0000',
-                fillOpacity: 0.35,
-              }}
-            />
-          )}
-        </GoogleMap>
-        {error && <div>{error}</div>}
-      </LoadScript>
-    )
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap mapContainerStyle={containerStyle} center={mapCenter} zoom={14}>
+        {driverMarker && <Marker position={driverMarker} />}
+        <Polygon
+          paths={zonePolygonPath}
+          options={polygonOptions}
+        />
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
